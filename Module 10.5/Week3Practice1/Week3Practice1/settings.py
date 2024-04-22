9da637d7917e4ef54c48746afffbdcac53ba22cb
Khushnor Rahman Meem <khushnorrahmanmeem@gmail.com>
2024-04-23 00:23:38 +0600

    10.5 completed

--- Module 10.5/Week3Practice1/Week3Practice1/settings.py
+++ Module 10.5/Week3Practice1/Week3Practice1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sample_app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Week3Practice1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Module 10.5/Week3Practice1/sample_app/views.py
+++ Module 10.5/Week3Practice1/sample_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from datetime import date,datetime
 
-# Create your views here.
+def index(request):
+    return render(request,'sample_app/index.html')
+
+def filter(request):
+    data ={
+        'name':"khushnor rahman meem",
+        'id' : "5971",
+        'gender':"female",
+        'department':"cs'e",
+        'DOB':date(2000, 5, 1),
+        'drama':['k2','mighty','flower','midnight'],
+        'empty':"",
+        'val':[
+            {'name': 'zed', 'age': 19},
+            {'name': 'amy', 'age': 22},
+            {'name': 'joe', 'age': 31},
+        ],
+        'li':{'a','b','c','d','e'},
+        'date':datetime.now()
+    }
+    return render(request,'sample_app/filter.html',data)
